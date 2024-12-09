6905af4a62c11669412aacb29b3b3c5ea57fa364
vladcheck <valekzhaninvladimir5@gmail.com>
2024-12-09 22:09:56 +0300

    Add button for deleting experience entry

--- src/components/ExperienceInputGroup.jsx
+++ src/components/ExperienceInputGroup.jsx
@@ -1,9 +1,13 @@
 import { InputRow } from "./InputRow";
+import { Button } from "./Button";
 
 export default function ExperienceInputGroup() {
   return (
     <div className="input-group">
-      <h3>Experience</h3>
+      <header className="experience-header">
+        <h3>Experience</h3>
+        <Button name="Remove this entry" />
+      </header>
       <InputRow name="Company full name" inputName="company" placeholder="Google LTD" required={true} />
       <InputRow name="Position" inputName="position" placeholder="CI/CD specialist" required={true} />
       <InputRow type="date" name="Date from" inputName="data-from" required={true} />
